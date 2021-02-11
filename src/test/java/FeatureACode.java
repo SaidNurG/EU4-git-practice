74e0ee3742cec1db294d2b7f0c3f5bd091f49d35
SaidNurG <saidnurgoekce@gmail.com>
2021-02-12 00:49:55 +0100

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,5 +3,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
+        System.out.println("EU4 is learning more Git with review");
     }
 }
